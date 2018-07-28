d7b60e44ad51b834166a9c0fb069bfb4f11cdd13
Raj Kumar <rajkumar.r@aapc.com>
2018-07-28 19:19:10 +0530

    First Commit

---
+++ IndexedDB/IndexedDB/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IndexedDB.Global" Language="C#" %>
---
+++ IndexedDB/IndexedDB/Js/indexeddb.js
@@ -0,0 +1,243 @@
+idb = {
+    idbObject: null, // windows indexedDB object.
+    idbtran: null, // windows transaction object.
+    dbRequest: null, // db creation request.
+    db: null, //database
+    version: 1, // database version
+    tables: null, // collection of object store.
+    init: function (options) {
+        if ('indexedDB' in window) {
+            idb.idbObject = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
+            idb.idbtran = window.IDBTransaction || window.webkitIDBTransaction;
+            idb.tables = options.tables;
+
+            var idbRequest = window.indexedDB.open(options.database, options.version); // open/create db with specific version
+            idbRequest.onerror = function () {
+                console.log("Error opening database.");
+            };
+
+            idbRequest.onsuccess = function (e) { // store success db object in order for curd.
+                idb.db = this.result;
+                idb.version = options.version;
+            };
+            idbRequest.onupgradeneeded = function (event) { // creation of object store first time on version change.
+                var resultDb = event.target.result;
+                idb.db = resultDb;
+                var optionTables = idb.tables;
+
+
+                //drop unwanted tables
+                for (var i = 0; i < resultDb.objectStoreNames.length; i++) {
+                    var needToDrop = true;
+                    for (var j = 0; j < optionTables.length; j++) {
+                        if (resultDb.objectStoreNames[i] == optionTables[j].name) {
+                            needToDrop = false;
+                            break;
+                        }
+                    }
+                    if (needToDrop) {
+                        idb.db.deleteObjectStore(resultDb.objectStoreNames[i]);
+                    }
+                }
+
+
+                //create new tables
+                for (var i = 0; i < optionTables.length; i++) {
+                    if (!resultDb.objectStoreNames.contains(optionTables[i].name)) {
+                        resultDb.createObjectStore(optionTables[i].name, { keyPath: optionTables[i].keyPath, autoIncrement: optionTables[i].autoIncrement });
+                        console.log(optionTables[i].name + " Created.");
+                    }
+                }
+
+
+
+
+
+            }
+        }
+        else {
+            console.log("This browser doesn't support IndexedDB");
+        }
+    },
+    insert: function (table, data, callback = null) {
+        var db = idb.db;
+
+        var isTableExists = false;
+        for (var i = 0; i < idb.tables.length; i++) {
+            if (idb.tables[i].name == table) {
+                isTableExists = true;
+                break;
+            }
+        }
+
+        if (!isTableExists) {
+            if (callback && typeof (callback) === "function") {
+                callback(false, table + " Table not found.");
+            }
+        }
+        else {
+            var tx = db.transaction(table, "readwrite");
+            var store = tx.objectStore(table);
+
+
+            var dataLength = 1;
+            if (data.constructor === Array) {
+                dataLength = data.length;
+                for (var i = 0; i < dataLength; i++) {
+                    store.put(data[i]);
+                }
+            }
+            else {
+                store.put(data);
+            }
+
+            tx.oncomplete = function () {
+                if (callback && typeof (callback) === "function") {
+                    callback(true, "" + dataLength + " records inserted.");
+                }
+            };
+
+        }
+    },
+    delete: function (table, key, callback) {
+        var db = idb.db;
+
+        var isTableExists = false;
+        for (var i = 0; i < idb.tables.length; i++) {
+            if (idb.tables[i].name == table) {
+                isTableExists = true;
+                break;
+            }
+        }
+
+        if (!isTableExists) {
+            if (callback && typeof (callback) === "function") {
+                callback(false, table + " Table not found.");
+            }
+        }
+        else {
+
+
+
+            var tx = db.transaction(table, "readwrite");
+            var store = tx.objectStore(table);
+
+            var keyLength = -1;
+            if (key && typeof (key) === "function") {
+                store.clear();
+            }
+            else {
+                if (key.constructor === Array) {
+                    keyLength = key.length
+                    for (var i = 0; i < keyLength; i++) {
+                        store.delete(key[i]);
+                    }
+                }
+                else {
+                    keyLength = 1;
+                    store.delete(key);
+                }
+            }
+
+
+            tx.oncomplete = function (event) {
+                //if all argument available
+                if (callback && typeof (callback) === "function") {
+                    callback(true, "" + keyLength == -1 ? "All" : keyLength + " records deleted.");
+                }
+
+                //if only two argument available
+                if (key && typeof (key) === "function") {
+                    key(true, "" + (keyLength == -1 ? "All" : keyLength) + " records deleted.");
+                }
+            };
+
+            tx.onerror = function () {
+                if (callback && typeof (callback) === "function") {
+                    callback(false, tx.error);
+                }
+            };
+        }
+    },
+    select: function (table, key, callback) {
+        var db = idb.db;
+
+        var isTableExists = false;
+        for (var i = 0; i < idb.tables.length; i++) {
+            if (idb.tables[i].name == table) {
+                isTableExists = true;
+                break;
+            }
+        }
+
+        if (!isTableExists) {
+            if (callback && typeof (callback) === "function") {
+                callback(false, table + " Table not found.");
+            }
+        }
+        else {
+
+            var tx = db.transaction(table, "readonly");
+            var store = tx.objectStore(table);
+            var request;
+            var keyLength = -1;
+            var data;
+            if (key && typeof (key) === "function") {
+                request = store.getAll();
+            }
+            else if (key.constructor === Array) {
+                keyLength = key.length
+                request = store.getAll();
+            }
+            else {
+                keyLength = 1;
+                request = store.get(key);
+            }
+
+
+            tx.oncomplete = function (event) {
+                //if all argument available
+                var result = request.result;
+                var keypath = request.source.keyPath;
+                var filteredResult = [];
+
+                //if need to filter key array
+                if (keyLength > 1) {
+                    for (var i = 0; i < result.length; i++) {
+                        for (var j = 0; j < keyLength; j++) {
+                            if (result[i][keypath] == key[j]) {
+                                filteredResult.push(result[i]);
+                                break;
+                            }
+                        }
+                    }
+                    result = filteredResult;
+                }
+
+
+                if (callback && typeof (callback) === "function") {
+                    callback(true, result);
+
+                }
+
+                //if only two argument available
+                if (key && typeof (key) === "function") {
+                    key(true, request.result);
+                }
+            }
+
+            tx.onerror = function () {
+                if (callback && typeof (callback) === "function") {
+                    callback(false, request.error);
+                }
+            };
+        }
+    },
+};
+
+
+
+
+
+
+
